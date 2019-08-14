baa2d58e0f4e98cb69b7962407295c06773dfe01
MarkFarwell94 <markfarwell94@gmail.com>
2019-08-14 14:35:47 +0100

    Initial Commit

--- PyClips/Clip Scraper/Clip Scraper.py
+++ PyClips/Clip Scraper/Clip Scraper.py
@@ -1,13 +1,20 @@
 from twitch import TwitchClient
+import time
 
 client = TwitchClient(client_id='zlj6axia46p0bmv64275aj3jjdbf13')
 
 # clips.get_top docs
 # https://python-twitch-client.readthedocs.io/en/latest/v5/clips.html
-channel_info = client.clips.get_top(channel='koil', period='week', limit=100)
+channel_info = client.clips.get_top(channel='koil', period='week', limit=20)
 
-# write clip url to text file
+# clips are output as 'https://clips.twitch.tv/KathishSweetWhalePrimeMe?tt_medium=clips_api&tt_content=url'
+# download link needs to split on '?'. Everything after '?' isn't needed
+# we split at '?'[0] and write to text file
+
+clip_urls = []
+ddmmyyyy = time.strftime("%d-%m-%y") # amazing variable name (example: dd-mm-yy)
 for item in channel_info:
-    with open('clips.txt', 'a') as file:
+    item['url'] = item['url'].split('?')[0] # take everything before '?'
+    with open(f'Downloaded Clips {ddmmyyyy}.txt', 'a') as file:
         file.writelines(f"{item['url']}\n")
         print(item['url'])
--- README.md
+++
@@ -1,9 +0,0 @@
-# PyClips
-
-Description
-===========
-Python source for project.
-
-TO-DO
-=====
-Add symlink between README in base and doc?
